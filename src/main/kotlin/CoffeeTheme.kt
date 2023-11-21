b8e8cfcb987e27cf1297ef6ccc20e0ebe393e911
Ikrom <74912914+Ikrom27@users.noreply.github.com>
2023-11-21 13:23:59 +0300

    color update

--- src/main/kotlin/CoffeeTheme.kt
+++ src/main/kotlin/CoffeeTheme.kt
@@ -3,6 +3,6 @@ import androidx.compose.ui.graphics.Color
 object CoffeeTheme {
     val primary = Color(127, 73, 41)
     val secondary = Color(165, 121, 56)
-    val backgroundSecondary = Color(227, 223, 212)
-    val background = Color(235, 233, 226)
+    val backgroundSecondary = Color(230, 198, 164)
+    val background = Color(243, 229, 210)
 }
